a9ed851251825677b7d7d8a98f2af1701c221218
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2020-05-18 18:12:39 -0300

    Add new app.get and res.send messages

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,10 +2,29 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Olá Mundo!\n');
+});
+
+app.get('/mars', function (req, res) {
+  res.send('Olá Marte!\n');
+});
+
+app.get('/Isadora', function (req, res) {
+  res.send('Olá Isadora, tudo bem? Você tem 8 anos, mas completará 9 aninhos no dia 11 de junho. Você está crescendo e está maravilhosa. Continue estudando, não pare de ler os livros e foque em matemática. Seus pais te amam demais!!!\n');
+});
+
+app.get('/Gabriel', function (req, res) {
+  res.send('Oi Biel, você está bem? Estou sabendo que você acabou de completar 2 aninhos ... uau ... O seu bolo de chocolate da Patrulha Canina com os personagens estava uma delícia, certo?\n');
+});
+
+app.get('/Joao', function (req, res) {
+  res.send('Como você está João? Logo, você completará 5 anos. Você está cuidando bem da vovó. O Gabriel e a Isadora estão morrendo de saudades de todos vocês.\n');
+});
+
+app.get('/Joao', function (req, res) {
+  res.send('Como você está João? Logo, você completará 5 anos. Você está cuidando bem da vovó. O Gabriel e a Isadora estão morrendo de saudades de todos vocês.\n');
 });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
-
